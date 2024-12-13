edab86f00f5a087c4b6d26a5d73e062955997e72
DorababuPragada <Dorababu_pragada@comcast.com>
2024-12-13 17:15:51 +0530

    Update dsVideoDevice.c

--- dsVideoDevice.c
+++ dsVideoDevice.c
@@ -26,38 +26,70 @@
 dsError_t  dsVideoDeviceInit()
 {
 	dsError_t ret = dsERR_NONE;
+	if(true == _bVideoDeviceInited)
+        {
+                return dsERR_ALREADY_INITIALIZED;
+	}
+	_bVideoDeviceInited = true;
 	return ret;
 }
 
 dsError_t  dsGetVideoDevice(int index, intptr_t *handle)
 {
 	dsError_t ret = dsERR_NONE;
-
-	*handle = 0;
+	if(false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
+	if (index != 0 || NULL == handle)
+	{
+		ret = dsERR_INVALID_PARAM;
+	}
 	return ret;
 }
 
 dsError_t  dsSetDFC(intptr_t handle, dsVideoZoom_t dfc)
 {
-	dsError_t ret = dsERR_NONE;
-
-	return ret;
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
+	if(!dsIsValidHandle(handle))
+	{
+		return dsERR_INVALID_PARAM;
+	}
+	return dsERR_OPERATION_NOT_SUPPORTED;
 }
 
 dsError_t  dsGetDFC(intptr_t handle, dsVideoZoom_t *dfc)
 {
-	dsError_t ret = dsERR_NONE;
-
-	return ret;
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
+	if (NULL == dfc || !dsIsValidHandle(handle))
+	{
+		 return dsERR_INVALID_PARAM;
+	}
+	 return dsERR_OPERATION_NOT_SUPPORTED;
 }
 
 dsError_t  dsVideoDeviceTerm()
 {
 	dsError_t ret = dsERR_NONE;
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
+	_bVideoDeviceInited = false;
 	return ret;
 }
 dsError_t dsGetHDRCapabilities(intptr_t handle, int *capabilities)
 {
+	if (false == _bVideoDeviceInited)
+	 {
+		  return dsERR_NOT_INITIALIZED;
+	 }
 	if(capabilities == NULL || !dsIsValidHandle(handle))
 	{
 	   return dsERR_INVALID_PARAM;
@@ -66,6 +98,10 @@ dsError_t dsGetHDRCapabilities(intptr_t handle, int *capabilities)
 }
 dsError_t dsGetSupportedVideoCodingFormats(intptr_t handle, unsigned int * supported_formats)
 {
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
 	if(supported_formats == NULL || !dsIsValidHandle(handle))
 	{
 		return dsERR_INVALID_PARAM;
@@ -74,6 +110,10 @@ dsError_t dsGetSupportedVideoCodingFormats(intptr_t handle, unsigned int * suppo
 }
 dsError_t dsGetVideoCodecInfo(intptr_t handle, dsVideoCodingFormat_t codec, dsVideoCodecInfo_t * info)
 {
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
 	if(info == NULL || !dsIsValidHandle(handle))
         {
            return dsERR_INVALID_PARAM;
@@ -82,6 +122,10 @@ dsError_t dsGetVideoCodecInfo(intptr_t handle, dsVideoCodingFormat_t codec, dsVi
 }
 dsError_t dsForceDisableHDRSupport(intptr_t handle, bool disable)
 {
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
 	if(!dsIsValidHandle(handle))
 	{
                 return dsERR_INVALID_PARAM;
@@ -90,6 +134,10 @@ dsError_t dsForceDisableHDRSupport(intptr_t handle, bool disable)
 }
 dsError_t dsSetFRFMode(intptr_t handle, int frfmode)
 {
+	if (false == _bVideoDeviceInited)
+	 {
+		 return dsERR_NOT_INITIALIZED;
+	 }
 	if(!dsIsValidHandle(handle) || frfmode < 0)
 	{
 		return dsERR_INVALID_PARAM;
@@ -98,6 +146,10 @@ dsError_t dsSetFRFMode(intptr_t handle, int frfmode)
 }
 dsError_t dsGetFRFMode(intptr_t handle, int *frfmode)
 {
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
         if(!dsIsValidHandle(handle) || frfmode == NULL)
         {
                 return dsERR_INVALID_PARAM;
@@ -106,6 +158,10 @@ dsError_t dsGetFRFMode(intptr_t handle, int *frfmode)
 }
 dsError_t dsGetCurrentDisplayframerate(intptr_t handle, char *framerate)
 {
+	if (false == _bVideoDeviceInited)
+	{
+		return dsERR_NOT_INITIALIZED;
+	}
         if(!dsIsValidHandle(handle) ||framerate == NULL)
         {
                 return dsERR_INVALID_PARAM;
@@ -114,6 +170,10 @@ dsError_t dsGetCurrentDisplayframerate(intptr_t handle, char *framerate)
 }
 dsError_t dsSetDisplayframerate(intptr_t handle, char *framerate)
 {
+	if (false == _bVideoDeviceInited)
+	{
+		 return dsERR_NOT_INITIALIZED;
+	}
      	if(!dsIsValidHandle(handle) ||framerate == NULL)
         {
                 return dsERR_INVALID_PARAM;
